a60495deda95c92c56bb2b99a58230d751393c51
tiinavilu <tiinavilu73@gmail.com>
2023-03-09 13:50:26 +0200

    rest-apifrom done

---
+++ src/main/java/com/sda/study/springbootpractice/configurations/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.sda.study.springbootpractice.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Tiina Raudla
+ * @Date
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT");
+    }
+}
